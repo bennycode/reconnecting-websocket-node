d2a9591197a0348d00ae96c820044e8d4b5b00bb
Benny Neugebauer <bn@bennyn.de>
2018-11-26 17:26:30 +0100

    Demo project

---
+++ src/WebSocketClient.ts
@@ -0,0 +1,35 @@
+import ReconnectingWebSocket, {Options} from 'reconnecting-websocket';
+import NodeWebSocket = require('ws');
+
+const options: Options = {
+  WebSocket: typeof window !== 'undefined' ? WebSocket : NodeWebSocket,
+  connectionTimeout: 4000,
+  debug: true,
+  maxReconnectionDelay: 10000,
+  maxRetries: Infinity,
+  minReconnectionDelay: 4000,
+  reconnectionDelayGrowFactor: 1.3,
+};
+
+function buildWebSocketURL(timestamp: number = Date.now()): string {
+  return `wss://echo.websocket.org/?time=${timestamp}`;
+}
+
+const socket: ReconnectingWebSocket = new ReconnectingWebSocket(
+  buildWebSocketURL,
+  undefined,
+  options
+);
+
+socket.onmessage = (event: MessageEvent): void => {
+  console.log(`Response: ${event.data}`);
+};
+
+socket.onerror = (): void => {
+  console.error('Ooops!');
+};
+
+socket.onopen = (): void => {
+  console.log('WebSocket is alive!');
+};
+

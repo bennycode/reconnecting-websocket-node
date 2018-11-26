71c66c1675cb74d09f6ac784998d3e6f7de8012c
Benny Neugebauer <bn@bennyn.de>
2018-11-26 18:39:58 +0100

    Cannot use "extends EventEmitter" with "esModuleInterop: true"

--- src/WebSocketClient.ts
+++ src/WebSocketClient.ts
@@ -1,35 +1,42 @@
 import ReconnectingWebSocket, {Options} from 'reconnecting-websocket';
 import NodeWebSocket = require('ws');
+import * as EventEmitter from "events";
 
-const options: Options = {
-  WebSocket: typeof window !== 'undefined' ? WebSocket : NodeWebSocket,
-  connectionTimeout: 4000,
-  debug: true,
-  maxReconnectionDelay: 10000,
-  maxRetries: Infinity,
-  minReconnectionDelay: 4000,
-  reconnectionDelayGrowFactor: 1.3,
-};
+class WebSocketClient extends EventEmitter {
+  connect(): void {
+    const options: Options = {
+      WebSocket: typeof window !== 'undefined' ? WebSocket : NodeWebSocket,
+      connectionTimeout: 4000,
+      debug: true,
+      maxReconnectionDelay: 10000,
+      maxRetries: Infinity,
+      minReconnectionDelay: 4000,
+      reconnectionDelayGrowFactor: 1.3,
+    };
 
-function buildWebSocketURL(timestamp: number = Date.now()): string {
-  return `wss://echo.websocket.org/?time=${timestamp}`;
+    function buildWebSocketURL(timestamp: number = Date.now()): string {
+      return `wss://echo.websocket.org/?time=${timestamp}`;
+    }
+
+    const socket: ReconnectingWebSocket = new ReconnectingWebSocket(
+      buildWebSocketURL,
+      undefined,
+      options
+    );
+
+    socket.onmessage = (event: MessageEvent): void => {
+      console.log(`Response: ${event.data}`);
+    };
+
+    socket.onerror = (): void => {
+      console.error('Ooops!');
+    };
+
+    socket.onopen = (): void => {
+      console.log('WebSocket is alive!');
+    };
+  }
 }
 
-const socket: ReconnectingWebSocket = new ReconnectingWebSocket(
-  buildWebSocketURL,
-  undefined,
-  options
-);
-
-socket.onmessage = (event: MessageEvent): void => {
-  console.log(`Response: ${event.data}`);
-};
-
-socket.onerror = (): void => {
-  console.error('Ooops!');
-};
-
-socket.onopen = (): void => {
-  console.log('WebSocket is alive!');
-};
-
+const myWebSocket = new WebSocketClient();
+myWebSocket.connect();
